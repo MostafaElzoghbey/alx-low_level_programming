0afa5a2ff003789be6055e8e2ccbcbbb06cf33ac
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-15 18:12:04 +0000

    done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 48; x < 58; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
