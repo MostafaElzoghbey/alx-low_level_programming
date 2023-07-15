1247035ab2289d6108136565cbd8ad0557b33be4
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-15 21:27:20 +0000

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x1;
+	int x2;
+
+	for (x2 = 0; x2 < 9; x2++)
+	{
+		for (x1 = 0; x1 < 10; x1++)
+		{
+			if (x1 > x2)
+			{
+				putchar('0' + x2);	
+				putchar('0' + x1);
+				if (x1 != 9 || x2 != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
