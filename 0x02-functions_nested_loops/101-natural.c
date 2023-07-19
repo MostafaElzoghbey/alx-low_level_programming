873203b0754e949e2f9ca4cb1c2a51fbcbfa2a2a
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-19 23:51:05 +0000

    done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -5,6 +5,18 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
+	int n1, n2, r;
+
+	for (n1 = 3; n1 <= 1024; n1 *= 3)
+	{
+		r += n1;
+	}
+	for (n2 = 5; n2 <= 1024; n2 *= 5)
+	{
+		r += n2;
+	}
+	printf("%d", r);
+
 	return (0);
 }
