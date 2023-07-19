59ee8a49185198408827fb86c40ca824af851d21
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-19 06:23:10 +0000

    done

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,11 +7,18 @@
  */
 int print_last_digit(int n)
 {
-	if (n < 0)
-	{
-		n = -n;
-	}
-	_putchar(n % 10 + '0');
+	int a;
 
-	return (n % 10);
+	if (n < 0)
+		n = -n;
+
+	a = n % 10;
+
+	if (a < 0)
+		a = -a;
+
+	_putchar(a + '0');
+
+	return (a);
 }
+
