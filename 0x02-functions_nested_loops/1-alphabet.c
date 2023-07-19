6b3383bc036d85f2bee07005fbe390439649306f
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-19 00:20:52 +0000

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include "_putchar.c"
 
 /**
  * print_alphabet - prints alphabet
@@ -7,31 +6,16 @@
  */
 void print_alphabet(void)
 {
-	_putchar('a');
-	_putchar('b');
-	_putchar('c');
-	_putchar('d');
-	_putchar('e');
-	_putchar('f');
-	_putchar('g');
-	_putchar('h');
-	_putchar('i');
-	_putchar('j');
-	_putchar('k');
-	_putchar('l');
-	_putchar('m');
-	_putchar('n');
-	_putchar('o');
-	_putchar('p');
-	_putchar('q');
-	_putchar('r');
-	_putchar('s');
-	_putchar('t');
-	_putchar('u');
-	_putchar('v');
-	_putchar('w');
-	_putchar('x');
-	_putchar('y');
-	_putchar('z');
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+	_putchar(c);
+	c++;
+	}
 	_putchar('\n');
+}
+void main(void)
+{
+	print_alphabet();
 }
