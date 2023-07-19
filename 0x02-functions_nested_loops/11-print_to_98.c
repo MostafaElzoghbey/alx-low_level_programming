87c5046b044fdc161ab3768f7214c5438a5e5370
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-19 22:56:41 +0000

    done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,11 +1,73 @@
-#include "stdio.h"
+#include "main.h"
 
 /**
  * main - do something
  * Return: Always 0 (Success)
  */
-int main(void)
+void print_to_98(int n)
 {
-	return (0);
+	if (n == 98)
+	{
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else if (n < 98)
+	{
+		for (; n < 0; n++)
+		{
+			if (n <= -10)
+			{
+			_putchar('-');
+			_putchar((-n / 10) + '0');
+			_putchar((-n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+			}
+			else
+			{
+				_putchar('-');
+				_putchar(-n + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		for (; n < 10; n++)
+		{
+			_putchar(n + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		for (; n <= 98; n++)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			if (n != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+	else if (n > 98)
+	{
+		for (; n >= 100; n--)
+		{
+			_putchar((n / 100) + '0');
+			_putchar(((n - 100) / 10) + '0');
+			_putchar((n % 10) + '0');
+			_putchar(',');
+			_putchar(' ');
+		}
+		for (; n >= 98; n--)
+		{
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+			if (n != 98)
+			{
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+	}
+	_putchar('\n');
 }
-
