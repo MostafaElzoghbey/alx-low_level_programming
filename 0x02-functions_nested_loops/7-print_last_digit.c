e3ebc43f390c74643aeb77e2943b78c80a2f8adb
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-19 01:41:15 +0000

    done

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}
