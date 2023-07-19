8ffc787c5fffab7779a36b44e53e01efb9093c65
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-19 05:41:07 +0000

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,9 +1,15 @@
-#include "stdio.h"
+#include "main.h"
+#include "_putchar.c"
 
 /**
  * main - do something
  * Return: Always 0 (Success)
  */
+void jack_bauer(void)
+{
+    int 
+    _putchar()
+}
 int main(void)
 {
     return (0);
