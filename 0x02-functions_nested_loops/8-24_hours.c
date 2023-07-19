022e0ee9630d193e8dc75d00824ddf249dda140e
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-19 06:26:38 +0000

    done

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,24 +3,26 @@
 /**
  * jack_bauer - prints every minute of the day of Jack Bauer
  * starting from 00:00 to 23:59
- * Return: Always 0 (Success)
  */
 void jack_bauer(void)
 {
-    int i, j;
+	int i, j;
 
-	for (i = 0; i < 24; i++)
+	i = 0;
+
+	while (i < 24)
 	{
-		for (j = 0; j < 60; j++)
+		j = 0;
+		while (j < 60)
 		{
-			
-            _putchar((i / 10) + 48);
-            _putchar((i % 10) + 48);
-            _putchar(':');
-            _putchar((j / 10) + 48);
-            _putchar((j % 10) + 48);
-            _putchar('\n');
+			_putchar((i / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(':');
+			_putchar((j / 10) + '0');
+			_putchar((j % 10) + '0');
+			_putchar('\n');
+			j++;
 		}
+		i++;
 	}
 }
-
