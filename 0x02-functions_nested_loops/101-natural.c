9de127d4aec1220e7c875c4c89a68b2a2a47b210
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-20 00:13:00 +0000

    done

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,13 +6,13 @@
  */
 int main(void)
 {
-	unsigned long int i, sum3, sum5, r;
+	unsigned long int sum3, sum5, r;
+	int i;
 
-	i = 0;
 	sum3 = 0;
 	sum5 = 0;
 	r = 0;
-	
+
 	for (i = 3; i <= 1024; i ++)
 	{
 		if ((i % 3) == 0)
