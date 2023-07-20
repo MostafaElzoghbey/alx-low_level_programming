0947348ff64f9478ba524e3596a87a02f057cba5
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-20 03:10:13 +0000

    done

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,34 +1,3 @@
-// #include <stdio.h>
-
-// /**
-//  * main - do something
-//  * Return: Always 0 (Success)
-//  */
-// int main(void)
-// {
-// 	unsigned long int i, n1, n2, r;
-
-// 	n1 = 1;
-// 	n2 = 2;
-// 	printf("%lu, %lu, ", n1, n2);
-// 	for (i = 1; i <= 98; i++)
-// 	{
-// 		r = n1 + n2;
-// 		if (i != 98)
-// 		{
-// 			printf("%lu, ", r);
-
-// 		} else
-// 		{
-// 			printf("%lu", r);
-// 		}
-// 		n1 = n2;
-// 		n2 = r;
-// 	}
-// 	printf("\n");
-
-// 	return (0);
-// }
 #include <stdio.h>
 
 int main() {
