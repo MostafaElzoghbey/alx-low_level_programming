bc0abf3ccd8dc92a3aad2eea9c565ab9b33ed023
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-20 01:09:45 +0000

    done

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,6 +5,30 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{ 
+{
+	long int i, n1, n2, r, sum;
+
+	i = 0;
+	n1 = 1;
+	n2 = 2;
+	r = 0,
+	sum = 0;
+	while (r <= 4000000)
+	{
+		i += 1;
+		r = n1 + n2;
+		if (r <= 4000000)
+		{
+			if (i % 2 == 0)
+			{
+				sum += r;
+			}
+			n1 = n2;
+			n2 = r;
+		}
+	}
+	printf("%ld", sum);
+	printf("\n");
+
 	return (0);
 }
