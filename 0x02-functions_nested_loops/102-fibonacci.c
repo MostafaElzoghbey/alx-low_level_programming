ed6b5b3a82f0086a8d4b31462626c6e938a5eb88
MostafaElzoghbey <133715838+MostafaElzoghbey@users.noreply.github.com>
2023-07-20 00:49:10 +0000

    done

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,7 +4,21 @@
  * main - do something
  * Return: Always 0 (Success)
  */
-int main(void)
-{ 
+int main(int n)
+{
+	long int i, n1, n2, r;
+
+	n1 = 1;
+	n2 = 2;
+	printf("%ld, %ld, ", n1, n2);
+	for(i = 1; i <= 50; i++)
+	{
+		r = n1 + n2;
+		printf("%ld, ", r);
+		n1 = n2;
+		n2 = r;
+	}
+	printf("\n");
+
 	return (0);
 }
