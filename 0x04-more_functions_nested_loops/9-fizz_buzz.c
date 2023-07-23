4fd241a62bbd550d82546a2a1750bb234d5df9b0
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-23 06:03:20 +0300

    done

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,9 +2,32 @@
 
 /**
  * main - do something
- * Return: Always 0 (Success)
+ * Return: thing
  */
 int main(void)
 {
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+		putchar(' ');
+	}
+	putchar('\n');
 	return (0);
 }
