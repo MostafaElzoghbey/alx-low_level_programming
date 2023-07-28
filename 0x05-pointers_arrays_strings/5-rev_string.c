ca1a4e680caf6527d7fded467c168843ce1377e7
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-29 02:30:57 +0300

    done

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,24 @@
 #include "main.h"
 
 /**
- * Prototype - do something
- * @: int or char
+ * rev_string - do something
+ * @s: int or char
  * Return: some thing
  */
-void Prototype()
+void rev_string(char *s)
 {
+	int i;
+	int j = 0;
+	int *cop = *s;
+	int len = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = len - 1; i >= 0; i--)
+	{
+		s[i] = cop[j];
+		j++;
+	}
 }
