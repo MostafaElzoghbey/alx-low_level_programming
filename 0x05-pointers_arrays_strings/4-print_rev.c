cd132067de0694faab11e1f95585b05bdb5ebe85
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-29 02:23:09 +0300

    done

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
 /**
- * Prototype - do something
- * @: int or char
+ * print_rev - do something
+ * @s: int or char
  * Return: some thing
  */
-void Prototype()
+void print_rev(char *s)
 {
+	int i;
+	int len = 0;
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+	for (i = len; i <= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
