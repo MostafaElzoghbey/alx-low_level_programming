4eaabe46e0ed9c3530842e74d2cd3a3c79f84dcd
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-29 02:16:27 +0300

    done

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
- * Prototype - do something
+ * _puts - do something
  * @: int or char
  * Return: some thing
  */
-void Prototype()
+void _puts(char *str)
 {
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
 }
