744f15a6da2bd927390b9d7bbf1aa470de16c148
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-29 03:51:24 +0300

    done

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,11 +1,32 @@
 #include "main.h"
 
 /**
- * Prototype - do something
- * @: int or char
+ * puts_half - do something
+ * @str: int or char
  * Return: some thing
  */
-void Prototype()
+void puts_half(char *str)
 {
+	int len = 0;
+	int i;
 
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len++;
+	}
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = (len - 1) / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
 }
