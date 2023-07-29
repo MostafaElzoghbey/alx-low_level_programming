e0c2f12feefb48d270178d64b73f39119b0c0c6e
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-29 04:04:36 +0300

    done

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
- * Prototype - do something
- * @: int or char
+ * print_array - do something
+ * @a: pointer to array
+ * @n: int
  * Return: some thing
  */
-void Prototype()
+void print_array(int *a, int n)
 {
+	int i;
 
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d, ", a[i]);
+		}
+		else
+		{
+			printf("%d", a[i]);
+		}
+	}
+	_putchar('\n');
 }
