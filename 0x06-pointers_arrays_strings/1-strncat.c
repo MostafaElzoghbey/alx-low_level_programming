73d1e71099e9d6c8a9fe9f00d384db9476361659
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-31 02:45:40 +0300

    done

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,11 +1,28 @@
 #include "main.h"
 
 /**
- * Prototype - do something
- * @: int or char
- * Return: some thing
+ * _strncat - do something
+ * @dest: pointer to char
+ * @src: pointer to char
+ * @n: int
+ * Return: something
  */
-void Prototype()
+char *_strncat(char *dest, char *src, int n)
 {
-	
+	int len2 = 0;
+	int lenTotal = 0;
+
+	while (dest[lenTotal] != '\0')
+	{
+		lenTotal++;
+	}
+	while (src[len2] != '\0' && len2 < n)
+	{
+		dest[lenTotal] = src[len2];
+		len2++;
+		lenTotal++;
+	}
+	dest[lenTotal] = '\0';
+
+	return (dest);
 }
