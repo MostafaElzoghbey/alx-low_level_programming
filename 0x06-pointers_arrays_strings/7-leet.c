4927cf2c87e3ca37c03980c3f34ae1eada8e2462
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-01 02:45:13 +0300

    done

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,15 +7,31 @@
  */
 char *leet(char *str)
 {
-	int len = 0;
+	int i;
+	int len;
+	char *text = "aAeEoOtTlL";
+	char *code = "4433007711";
 
 	for (len = 0; str[len] != '\0'; len++)
 	{
-		if (str[len] == 'a' || str[len] == 'A')
+		for (i = 0; text[i] != '\0'; i++)
 		{
-			str[len] = 'a' - 'a' + '4';
+			if (str[len] == text[i])
+			{
+				str[len] = code[i];
+			}
 		}
 	}
 
 	return (str);
+}
+int main(void)
+{
+    char s[] = "Expect the best. Prepare for the worst. Capitalize on what comes.\n";
+    char *p;
+
+    p = leet(s);
+    printf("%s", p);
+    printf("%s", s);
+    return (0);
 }
