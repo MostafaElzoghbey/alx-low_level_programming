69dfa0eac5298efd3c3231f16426bfafbc484cfa
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-31 21:14:24 +0300

    done

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,25 @@
 #include "main.h"
 
 /**
- * Prototype - do something
- * @: int or char
+ * cap_string - do something
+ * @str: pointer to char
  * Return: some thing
  */
-void Prototype()
+char *cap_string(char *str)
 {
-	
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (!(str[len - 1] >= 'a' && str[len - 1] <= 'z'))
+		{
+			if (str[len] >= 'a' && str[len] <= 'z')
+			{
+				str[len] = str[len] - 32;
+			}
+		}	
+		len++;
+	}
+
+	return (str);
 }
