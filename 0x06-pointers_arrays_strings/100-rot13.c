0327ff263f527c8669c531449eb535c756e2c1f2
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-01 02:53:06 +0300

    done

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * rot13 - do something
+ * @str: pointer to char
+ * Return: some thing
+ */
+char *rot13(char *str)
+{
+	int i;
+	int len;
+	char *text = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char *code = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (len = 0; str[len] != '\0'; len++)
+	{
+		for (i = 0; text[i] != '\0'; i++)
+		{
+			if (str[len] == text[i])
+			{
+				str[len] = code[i];
+			}
+		}
+	}
+
+	return (str);
+}
