8ecb354bb2854e9bbaaaa2a32bb8f8b52a6ea008
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-07-31 03:29:25 +0300

    done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,22 @@
 #include "main.h"
 
 /**
- * Prototype - do something
- * @: int or char
+ * _strcmp - do something
+ * @s1: pointer to char
+ * @s2: pointer to char
  * Return: some thing
  */
-void Prototype()
+int _strcmp(char *s1, char *s2)
 {
-	
+	while (*s1 == *s2)
+	{
+		if (*s1 = '\0')
+		{
+			return (0);
+		}
+		*s1++;
+		*s2++;
+	}
+
+	return (*s1 - *s2);
 }
