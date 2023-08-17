16a0a32d1da397be7bc2a0590fb6f9dcf950450a
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-17 22:58:16 +0300

    done

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
 
 /**
- * print_numbers - prints numbers given as parameters
- * @separator: string to be printed between numbers
- * @n: number of integers passed to the function
+ * print_numbers - dosomething
+ * @separator: string
+ * @n: int
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
