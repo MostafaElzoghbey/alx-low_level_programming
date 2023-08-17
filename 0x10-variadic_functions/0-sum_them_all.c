087086419282e92758dfc0c76836d0fccc40c2d4
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-17 21:51:28 +0300

    done

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -14,13 +14,8 @@ int sum_them_all(const unsigned int n, ...)
 
 	va_start(list, n);
 
-	if (n != 0)
-	{
-		for (i = 0; i < n; i++)
-		{
-			sum += va_arg(list, int);
-		}
-	}
+	for (i = 0; i < n; i++)
+		sum += va_arg(list, int);
 
 	va_end(list);
 
