b9e615d8505ba254050be070d45dfa14a807b85f
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-26 02:02:04 +0300

    done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,12 +14,12 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return (NULL);
+		return (0);
 
 	p = (char *) malloc(sizeof(char) * size);
 
 	if (p == NULL)
-		return (NULL);
+		return (0);
 
 	while (i < size)
 	{
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strdup - copies the string given as parameter
+ * @str: string
+ * 
+ * Return: pointer to the copied string in (Success), NULL in (Error)
+*/
+char *_strdup(char *str)
+{
+
+}
