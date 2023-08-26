786a12e28e0097ab50894b7a1b9f9e20eda8277a
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-26 03:19:03 +0300

    done

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - free the memory allocate for the grid(two dimensional array)
+ * created by alloc_grid()
+ * @grid: grid to free
+ * @height: height of the grid(two dimensional array)
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (grid == NULL || height == 0)
+		return;
+	
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
