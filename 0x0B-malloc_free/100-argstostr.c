4bda095b57cd00a67d07cc587862e3699dbbcb53
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-08-26 03:51:01 +0300

    done

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+  * argstostr - concatenates all arguments of the program to one string
+  * @ac: argument count
+  * @av: pointer to array of size ac
+  * Return: Pointer to new total string or NULL
+  */
+char *argstostr(int ac, char **av)
+{
+	int height, width, totalLen;
+	char *str;
+
+	totalLen = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	height = 0;
+	while (height < ac)
+	{
+		width = 0;
+		while (av[height][width])
+		{
+			totalLen++;
+			width++;
+		}
+		totalLen++;
+		height++;
+	}
+	str = malloc((sizeof(char) * totalLen) + 1);
+	if (str == NULL)
+		return (NULL);
+	
+	totalLen = 0;
+	height = 0;
+	while (height < ac)
+	{
+		width = 0;
+		while (av[height][width])
+		{
+			str[totalLen] = av[height][width];
+			totalLen++;
+			width++;
+		}
+		str[totalLen] = '\n';
+		totalLen++;
+		height++;
+	}
+	str[totalLen] = '\0';
+
+	return (str);
+}
