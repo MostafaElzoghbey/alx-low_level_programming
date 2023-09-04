982360740dc34bc331495db8abb04f923d010bc9
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-09-04 18:52:30 +0300

    done

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit - set value of a bit to 1 at a given index
+ * @n: decimal number passed by pointer
+ * @index: index position to change to 1, starting from 0
+ * Return: 1 if it worked, -1 if error
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int powerOfOne;
+
+	if (index > 64)
+		return (-1);
+
+	for (p = 1; index > 0; index--, powerOfOne *= 2)
+		;
+	*n += powerOfOne;
+
+	return (1);
+}
