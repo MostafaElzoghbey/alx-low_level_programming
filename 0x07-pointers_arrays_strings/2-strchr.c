eec490d5e3cf351ba88c8ffc4a597aac29b4b73d
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-09-28 09:30:22 +0300

    done

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 
 /**
  * _strchr - a function that locates a character in a string
@@ -12,11 +13,10 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	while (1)
 	{
 		if (s[i] == c)
 			return (s + i);
 		i++;
 	}
-	return ('\0');
 }
