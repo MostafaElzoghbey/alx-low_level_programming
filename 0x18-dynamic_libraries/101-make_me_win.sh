4b655b844329ea455f68f7603b3254e407d07da2
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-10-17 05:03:29 +0300

    done

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/ollyimanishimwe/alx-low_level_programming/raw/main/0x18-dynamic_libraries/putshack.so
-export LD_PRELOAD=/tmp/putshack.so
+wget -O /tmp/libinjection.so https://github.com/MostafaElzoghbey/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libinjection.so
+export LD_PRELOAD=/tmp/libinjection.so
