725e6bdedcb3f9fed88e7824a434822f49698eb5
MostafaElzoghbey <mostafaelzoghbeywork1@gmail.com>
2023-10-17 05:01:04 +0300

    done

--- 0x07-pointers_arrays_strings/test.c
+++ 0x07-pointers_arrays_strings/test.c
@@ -2,9 +2,11 @@
 
 char *uniqe_str(char *s)
 {
-	char *uniqeStr = "";
-	int *index, i, j, flag, k = 0;
+	char *uniqeStr = s;
+	int i, j, flag, k = 0;
 
+	if (!s)
+		return (&s[0]);
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)
@@ -25,19 +27,24 @@ char *uniqe_str(char *s)
 	return (uniqeStr);
 }
 
-void main(void)
+int main(void)
 {
-	char *s = "asdzasd", *uniqeStr;
+	char *s = "", *uniqeStr;
 
 	puts("here");
 	uniqeStr = uniqe_str(s);
-	if (*uniqeStr == '\0')
+	puts("here");
+	printf("%s\n", uniqeStr);
+	/* if (*uniqeStr == '\0') 
 	{
-		// printf("%s\n", uniqeStr);
 		printf("NULL\n");
 	}
 	else
 	{
 		printf("NOT NULL\n");
 	}
+	 */
+	return (0);
 }
+
+
